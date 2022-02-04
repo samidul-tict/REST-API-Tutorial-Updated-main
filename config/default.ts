69d1ec0e85e3013a191e81d5e5e81ad8c5305b53
Sk Samidul Islam <samidul.tict@gmail.com>
2022-02-03 23:57:38 -0500

    first commit

---
+++ config/default.ts
@@ -0,0 +1,11 @@
+export default {
+  port: 1337,
+  dbUri: "mongodb+srv://compass_app:KhSt5ehn78WmPs@cluster0.xjqge.mongodb.net/test",
+  saltWorkFactor: 10,
+  accessTokenTtl: "60m",
+  refreshTokenTtl: "1y",
+  accessTokenPrivateKey: ``,
+  accessTokenPublicKey: ``,
+  refreshTokenPrivateKey: ``,
+  refreshTokenPublicKey: ``,
+};
